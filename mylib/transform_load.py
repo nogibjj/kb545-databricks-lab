b0ac7afd42af8054d5bd0b25b5bcd750b0b6674a
Ninsta22 <kbagherlee@gmail.com>
2023-10-01 18:10:17 -0400

    Linter Work NOW

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,7 +1,9 @@
 """
 Transforms and Loads data into the local SQLite3 database
 Example:
-,general name,count_products,ingred_FPro,avg_FPro_products,avg_distance_root,ingred_normalization_term,semantic_tree_name,semantic_tree_node
+,general name,count_products,ingred_FPro,avg_FPro_products\
+    ,avg_distance_root,ingred_normalization_term,\
+        semantic_tree_name,semantic_tree_node
 """
 import sqlite3
 import csv
@@ -19,7 +21,10 @@ def load(dataset="GroceryDB_IgFPro.csv"):
     c = conn.cursor()
     c.execute("DROP TABLE IF EXISTS GroceryDB")
     c.execute(
-        "CREATE TABLE GroceryDB (id,general_name, count_products, ingred_FPro, avg_FPro_products, avg_distance_root, ingred_normalization_term, semantic_tree_name, semantic_tree_node)"
+        "CREATE TABLE GroceryDB (id,general_name, count_products,\
+              ingred_FPro, avg_FPro_products, avg_distance_root, \
+                ingred_normalization_term, semantic_tree_name, \
+                    semantic_tree_node)"
     )
     # insert
     c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
