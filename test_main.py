0d6426adb1bf915736fbc5207a02bb38c150a7c1
Ninsta22 <kbagherlee@gmail.com>
2023-10-08 19:17:36 -0400

    Test fixes with linter

--- test_main.py
+++ test_main.py
@@ -7,7 +7,11 @@ from main import query, run_query
 class TestRunQuery(unittest.TestCase):
     @patch("sys.stdout", new_callable=StringIO)
     def test_run_query(self, mock_stdout):
-        expected_output = "[(u'Goran Dragic', 0), (u'Josh Richardson', -1), (u'Kelly Olynyk', -2), (u'Wayne Ellington', -3), (u'James Johnson', -4), (u'Justise Winslow', -5), (u'Hassan Whiteside', -6), (u'Dion Waiters', -7), (u'Bam Adebayo', -8), (u'Tyler Johnson', -9), (u'Derrick Jones Jr.', -10), (u'Rodney McGruder', -11)]\n"
+        expected_output = "[(u'Goran Dragic', 0), (u'Josh Richardson', -1), (u'Kelly Olynyk', -2),\
+              (u'Wayne Ellington', -3), (u'James Johnson', -4), (u'Justise Winslow', -5), \
+                (u'Hassan Whiteside', -6), (u'Dion Waiters', -7), (u'Bam Adebayo', -8), \
+                    (u'Tyler Johnson', -9), (u'Derrick Jones Jr.', -10), \
+                        (u'Rodney McGruder', -11)]\n"
         run_query(query)
         self.assertEqual(mock_stdout.getvalue(), expected_output)
 
