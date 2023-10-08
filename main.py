e0204cf96ecb50afd3038b3039436d7675913d42
Ninsta22 <kbagherlee@gmail.com>
2023-10-08 19:08:05 -0400

    Listened to Lint

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from databricks import sql
 from dotenv import load_dotenv
 import os
 
-query = """SELECT t1.Player, t1.FG - t2.FG AS FG_Change FROM nba__stats_2019 as t1 JOIN heat_2018 as t2 ON t1.Player = t2.Name
+query = """SELECT t1.Player, t1.FG - t2.FG AS FG_Change 
+FROM nba__stats_2019 as t1 JOIN heat_2018 as t2 ON t1.Player = t2.Name
 WHERE t1.Tm LIKE 'MIA'
 ORDER BY FG_Change DESC"""
 
